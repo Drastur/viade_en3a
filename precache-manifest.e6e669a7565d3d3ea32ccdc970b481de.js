db39f37c483bc7ce68367654a388efdf1fbc071a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-17 14:53:16 +0000

    Deploy Arquisoft/viade_en3a to github.com/Arquisoft/viade_en3a.git:gh-pages

---
+++ precache-manifest.e6e669a7565d3d3ea32ccdc970b481de.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfc728d6467d229f830577b950a94f0",
+    "url": "/viade_en3a/index.html"
+  },
+  {
+    "revision": "b05b75ec75e99cc8de4a",
+    "url": "/viade_en3a/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a0e738b56ecff2404ff0",
+    "url": "/viade_en3a/static/css/main.2ceffab3.chunk.css"
+  },
+  {
+    "revision": "b05b75ec75e99cc8de4a",
+    "url": "/viade_en3a/static/js/2.91bd3e68.chunk.js"
+  },
+  {
+    "revision": "90a0e44847506c13674aa74d24c2ad0f",
+    "url": "/viade_en3a/static/js/2.91bd3e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e738b56ecff2404ff0",
+    "url": "/viade_en3a/static/js/main.51c10d00.chunk.js"
+  },
+  {
+    "revision": "0ef98ab1311a97b98a5f",
+    "url": "/viade_en3a/static/js/runtime-main.382c944d.js"
+  },
+  {
+    "revision": "774f104ca940d2f5552017963355d94f",
+    "url": "/viade_en3a/static/media/car_img1.774f104c.jpeg"
+  },
+  {
+    "revision": "67c42e97ffed6ed12c9b53d927b2c4d5",
+    "url": "/viade_en3a/static/media/car_img2.67c42e97.jpg"
+  },
+  {
+    "revision": "a53326b60ed013077f577fe9dc6a1f05",
+    "url": "/viade_en3a/static/media/friend_profile_A.a53326b6.png"
+  },
+  {
+    "revision": "8308010346e95d7f13608bc0404d0339",
+    "url": "/viade_en3a/static/media/friend_profile_B.83080103.png"
+  },
+  {
+    "revision": "f2d0c1155cb254b380bd13893f600c50",
+    "url": "/viade_en3a/static/media/logo_alt.f2d0c115.jpeg"
+  },
+  {
+    "revision": "0a33663c6c648916347f41f6950142be",
+    "url": "/viade_en3a/static/media/logo_full.0a33663c.jpeg"
+  },
+  {
+    "revision": "511776fe6c6e981aed0ba3b6967a5ba6",
+    "url": "/viade_en3a/static/media/profile_img.511776fe.png"
+  }
+]);
